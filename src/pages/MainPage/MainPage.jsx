d31d6bb0029a8ccb8643847753c7b113bf8ab898
Katsiaryna Hanchar <kattie@outlook.com>
2024-07-28 20:46:43 +0200

    variables and fonts added, HomePage and MainPage added

---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,14 @@
+
+
+import { Header } from "../../components/Header";
+import { HeroSlider } from "../../components/HeroSlider";
+import { InStock } from "../../components/InStock";
+
+
+export const MainPage = () => {
+  return <>
+  <Header />
+  <HeroSlider />
+  <InStock />
+  </>;
+};
