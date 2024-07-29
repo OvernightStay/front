47293b7f112e74a39f6d2efee55000b048f403f6
Veronika <xafling@mail.ru>
2024-07-29 16:50:37 +0300

    tuned imports

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,14 +1,13 @@
-
-
-import { Header } from "../../components/Header";
-import { HeroSlider } from "../../components/HeroSlider";
-import { InStock } from "../../components/InStock";
-
+import { Header } from '../../components/Header';
+import { HeroSlider } from '../../components/HeroSlider';
+import { InStock } from '../../components/InStock';
 
 export const MainPage = () => {
-  return <>
-  <Header />
-  <HeroSlider />
-  <InStock />
-  </>;
+  return (
+    <>
+      <Header />
+      <HeroSlider />
+      <InStock />
+    </>
+  );
 };
